5bbb63298e38c0473ff1cb934604b2a348c3c36a
Duh-Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-07-17 13:12:07 -0400

    Start tab logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import renderHomePage from "./homePage";
 
 renderHomePage();
+
+
+const clearContent = () => {
+    const content = document.getElementById("content");
+    content.innerHTML = "";
+}
+
+//module pattern to attach event listeners to the appropriate buttons
+const attachEventListeners = (() => {
+    
+    const homeButton = document.getElementById("home-button");
+    homeButton.addEventListener('click', () => {
+        clearContent();
+        renderHomePage();
+    });
+
+    console.log(homeButton);
+
+    const menuButton = document.getElementById("menu-button");
+
+    console.log(menuButton);
+    
+    menuButton.addEventListener('click', clearContent);
+
+})();
